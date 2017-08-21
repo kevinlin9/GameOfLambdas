e3e22da1ed47345c56be744fd6e9536f7fde7749
Kevin Lin <klin7743@bths.edu>
2017-08-21 19:20:13 -0400

    Moved files for GitHub Pages hosting.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GameOfLambdas
 Quirky Spring Final Project for Web Development. Utilizes AJAX, CSS, HTML, and a bit of PHP. Includes some inside-jokes and memes.
+
+https://linkhua.github.io/GameOfLambdas/
--- Snake Game Project/snake_game_results.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$name = $_GET["name"];
-	$difficulty = $_GET["difficulty"];
-	$score = $_GET["score"];
-	$connection = new PDO("mysql:hostname=localhost;dbname=klin","root","");
-	$cmd =  "INSERT INTO `leaderboard` (`name`, `difficulty`, `score`) VALUES ('$name', '$difficulty', '$score')";
-	$action = $connection->prepare($cmd);
-	$action->execute();
-?> 
